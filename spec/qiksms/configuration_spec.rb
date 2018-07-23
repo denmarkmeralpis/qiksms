5c5351bf04166b982c8c6e37cfdaee29235d352a
Nujian Den Mark Meralpis <meralpisdenmark@gmail.com>
2018-07-23 11:51:11 +0800

    add tests

--- spec/qiksms/configuration_spec.rb
+++ spec/qiksms/configuration_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Qiksms::Configuration do
 		Qiksms.configure do |config|
 			config.api_key = ENV['QIKSMS_API_KEY']
 			config.api_secret = ENV['QIKSMS_API_SECRET']
+			config.sender_id = ENV['QIKSMS_SENDER_ID']
+			config.char_type = ENV['QIKSMS_CHAR_TYPE']
 		end
 	end
 
@@ -18,6 +20,14 @@ RSpec.describe Qiksms::Configuration do
 
 		it 'returns the correct qiksms api secret' do
 			expect(Qiksms.configuration.api_secret).to eq(ENV['QIKSMS_API_SECRET'])
+		end
+
+		it 'returns the correct qiksms sender id' do
+			expect(Qiksms.configuration.sender_id).to eq(ENV['QIKSMS_SENDER_ID'])
+		end
+
+		it 'returns the correct qiksms char type' do
+			expect(Qiksms.configuration.char_type).to eq(ENV['QIKSMS_CHAR_TYPE'])
 		end
 	end
 
@@ -30,8 +40,16 @@ RSpec.describe Qiksms::Configuration do
 			expect { Qiksms.configuration.api_key }.to raise_error(Qiksms::Errors::Configuration)
 		end
 
-		it 'raises a configuration error for secret' do
+		it 'raises a configuration error for api_secret' do
 			expect { Qiksms.configuration.api_secret }.to raise_error(Qiksms::Errors::Configuration)
+		end
+
+		it 'raises a configuration error for sender_id' do
+			expect { Qiksms.configuration.sender_id }.to raise_error(Qiksms::Errors::Configuration)
+		end
+
+		it 'raises a configuration error for char_type' do
+			expect { Qiksms.configuration.char_type }.to raise_error(Qiksms::Errors::Configuration)
 		end
 	end
 
@@ -39,10 +57,14 @@ RSpec.describe Qiksms::Configuration do
 		it 'resets configured values' do
 			expect(Qiksms.configuration.api_key).to eq(ENV['QIKSMS_API_KEY'])
 			expect(Qiksms.configuration.api_secret).to eq(ENV['QIKSMS_API_SECRET'])
+			expect(Qiksms.configuration.char_type).to eq(ENV['QIKSMS_CHAR_TYPE'])
+			expect(Qiksms.configuration.sender_id).to eq(ENV['QIKSMS_SENDER_ID'])
 
 			Qiksms.reset
 			expect { Qiksms.configuration.api_key }.to raise_error(Qiksms::Errors::Configuration)
 			expect { Qiksms.configuration.api_secret }.to raise_error(Qiksms::Errors::Configuration)
+			expect { Qiksms.configuration.char_type }.to raise_error(Qiksms::Errors::Configuration)
+			expect { Qiksms.configuration.sender_id }.to raise_error(Qiksms::Errors::Configuration)
 		end
 	end
 end
