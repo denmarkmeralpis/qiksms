f7479991536fa07725f2b9f49ea58995dd62dd5c
Nujian Den Mark Meralpis <meralpisdenmark@gmail.com>
2018-07-23 11:51:05 +0800

    add config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'bundler/setup'
 require 'qiksms'
+require 'byebug'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
+  config.expose_dsl_globally = true
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
@@ -16,6 +17,8 @@ RSpec.configure do |config|
     Qiksms.configure do |config|
       config.api_secret = ENV['QIKSMS_API_SECRET']
       config.api_key = ENV['QIKSMS_API_KEY']
+      config.char_type = ENV['QIKSMS_CHAR_TYPE']
+      config.sender_id = ENV['QIKSMS_SENDER_ID']
     end
   end
 end
